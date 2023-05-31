ee20b13af4d9e08d7c471bce1d15a8b11383e605
Ozair Ismail <ojismail@gmail.com>
2023-05-31 20:45:21 +0100

    Second exercise

---
+++ Basics/2.rb
@@ -0,0 +1,15 @@
+thousands = 5678 / 1000
+
+puts thousands
+
+hundreds = 5678 % 1000 / 100
+
+puts hundreds
+
+tens = 5678 % 1000 % 100 / 10
+
+puts tens
+
+ones = 5678 % 1000 % 100 % 10
+
+puts ones
