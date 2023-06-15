067bb9483c12ef2c3b27e99490f52e191fa59a0b
Ozair Ismail <ojismail@gmail.com>
2023-06-15 22:30:14 +0100

    Methods done

---
+++ Methods/1.rb
@@ -0,0 +1,5 @@
+def greeting(name)
+     "Salam " + name 
+end
+
+p greeting("Ozair")
---
+++ Methods/6.rb
@@ -0,0 +1,7 @@
+ArgumentError: wrong number of arguments (1 for 2)
+  from (irb):1:in `calculate_product'
+  from (irb):4
+  from /Users/username/.rvm/rubies/ruby-2.5.3/bin/irb:12:in `<main>'
+
+
+  2 arguments expected, only 1 given.
