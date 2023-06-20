38f54538e9a344ec9ee33f922d4dad390e1c818a
Ozair Ismail <ojismail@gmail.com>
2023-06-20 21:23:09 +0100

    Flow done

---
+++ Flow/2.rb
@@ -0,0 +1,9 @@
+def upcase(string)
+    if string.length >= 10 
+        string.upcase
+    else
+        string
+    end
+end 
+
+puts upcase("salam here I am bro")
---
+++ Flow/3.rb
@@ -0,0 +1,9 @@
+number = gets.chomp.to_i
+
+if number <= 50
+    puts "Stage 1"
+elsif number <= 100   
+    puts "Stage 2"
+else
+    puts "Stage 3"
+end
