6f84f56259510ef99dc66eec2f3bd4e7df682c40
Ozair Ismail <ojismail@gmail.com>
2023-07-02 04:40:30 +0100

    hashes

---
+++ Hashes/1.rb
@@ -0,0 +1,10 @@
+family = {  uncles: ["bob", "joe", "steve"],
+            sisters: ["jane", "jill", "beth"],
+            brothers: ["frank","rob","david"],
+            aunts: ["mary","sally","susan"]
+          }
+
+
+siblings =  family.select { | k, v | k == :sisters || k == :brothers}
+
+p array = siblings.values.flatten
---
+++ Hashes/2.rb
@@ -0,0 +1,10 @@
+name = {name: "whiskers"}
+age = {age: "one"}
+
+p name.merge(age)
+p name
+p age
+
+p name.merge!(age)
+p name
+p age 
